ebbcd445f08b928435faa3fe019a6f2d40dccd82
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2021-10-25 21:44:03 +0100

    initial commit

---
+++ background.js
@@ -0,0 +1,43 @@
+/* backgound.js */
+console.log("----- [backgound.js] LOADED");
+
+//chrome.runtime.onMessage.addListener(
+//	function (response, sender, sendResponse) {
+//		console.log("background message recieved")
+//		var opt = {
+//			type: "basic",
+//			title: response.reason,
+//			message: response.message,
+//			iconUrl: "/icons/256x256.png"
+//		}
+//		chrome.notifications.create("message", opt, (e) => console.log(e));
+//		sendResponse();
+//	}
+//);
+
+chrome.runtime.onMessage.addListener(
+	function (request, sender, sendResponse) {
+		console.log(sender.tab ?
+			"from a content script:" + sender.tab.url :
+			"from the extension");
+		if (request.greeting === "ADD") {
+			chrome.contextMenus.create({
+				title: 'ANKI',
+				onclick: function (e) {
+					chrome.tabs.query({
+						"active": true,
+						"currentWindow": true
+					}, function (tabs) {
+						chrome.tabs.sendMessage(tabs[0].id, {
+							"functiontoInvoke": "InstagramMessages"
+						});
+					});
+				},
+				contexts: ["selection"]
+
+			}, () => { });
+
+			sendResponse({ status: "complete" });
+		}
+	}
+);
---
+++ content_script.js
@@ -0,0 +1,212 @@
+/* This runs on all "youtube.com/watch" web pages */
+console.log("----- [content_script.js] LOADED");
+
+if (window.location.href.includes("instagram.com/direct")) {
+    console.log("Adding Right Click option to Instagram messenger")
+
+    chrome.runtime.sendMessage({ greeting: "ADD" }, function (response) {
+        console.log(response.farewell);
+    });
+}
+
+chrome.extension.onMessage.addListener(function (message, sender, callback) {
+    if (message.functiontoInvoke == "InstagramMessages") {
+        InstagramMessages();
+    }
+});
+
+function InstagramMessages() {
+    console.log("[InstagramMessages] Start...")
+
+    const selection = window.getSelection()
+    const selected_word = selection.toString()
+
+    const parent_of_selected = selection.anchorNode.parentNode
+    let message_current_text = parent_of_selected.innerText
+    message_current_text = message_current_text.replace(selected_word, "<b>" + selected_word + "</b>");
+
+    console.log({ message_current_text })
+
+    const root_element = parent_of_selected.parentNode.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
+
+    let messages = [];
+    let node_above = root_element.previousSibling
+
+    const message_side = getComputedStyle(root_element.childNodes[1].children[0].children[0]).alignSelf;
+
+    messages.push([message_current_text, message_side])
+
+    // qF0y9
+
+    for (let index = 0; index < 3; index++) {
+        const node_with_css = node_above.childNodes[1].children[0].children[0]
+        const chat_side = getComputedStyle(node_with_css).alignSelf;
+
+        messages.push([node_above.innerText, chat_side])
+
+        node_above = node_above.previousSibling
+    }
+
+    //messages.forEach(e => { console.log(e) });
+
+
+    console.log("[InstagramMessages] End")
+
+    GenerateChatHTML(messages)
+}
+
+function GenerateChatHTML(message_details) {
+    console.log("[GenerateChatHTML] Start...")
+
+    var base_HTML = "<style>\
+        .center {\
+            width: 50%;\
+            margin: 0 auto;\
+            font-family: Red hat Display, sans-serif;\
+            font-weight: 400;\
+            line-height: 1.5em;\
+            letter-spacing: 0.025em;\
+            color: #333;\
+            background: #f7f7f7;\
+        }\
+        .message {\
+            font-size: 0.9rem;\
+            color: #999;\
+        }\
+        .chat {\
+            position: relative;\
+            border-radius: 1rem;\
+            background: white;\
+            border-radius: 1.125rem 1.125rem 1.125rem 1.125rem;\
+        }\
+        .chat .messages {\
+            padding: 1rem;\
+            background: #f7f7f7;\
+            flex-shrink: 2;\
+            overflow-y: auto;\
+        }\
+        .chat .messages .message {\
+            box-sizing: border-box;\
+            padding: 0.5rem 1rem;\
+            margin: 1rem;\
+            background: #fff;\
+            border-radius: 1.125rem 1.125rem 1.125rem 0;\
+            min-height: 2.25rem;\
+            width: -webkit-fit-content;\
+            width: -moz-fit-content;\
+            width: fit-content;\
+            max-width: 66%;\
+            box-shadow: 0 0 2rem rgba(0, 0, 0, 0.075), 0rem 1rem 1rem -1rem rgba(0, 0, 0, 0.1);\
+        }\
+        .chat .messages .message.right {\
+            margin: 1rem 1rem 1rem auto;\
+            border-radius: 1.125rem 1.125rem 0 1.125rem;\
+            background: #333;\
+            color: white;\
+        }\
+    </style>\
+        <div class=\"center\">\
+            <div class=\"chat\">\
+                <div class=\"messages\" id=\"chat\">"
+
+    // left message  = 'flex-start'
+    // right message = 'flex-end'
+    //<div class="message right"></div>
+    //<div class="message left"></div>
+
+    for (var i = message_details.length - 1; i >= 0; i--) {
+        if (m[i][1] === 'flex-start') {
+            base_HTML += "<div class=\"message left\">" + m[i][0] + "</div>"
+        }
+        else if (m[i][1] === 'flex-end') {
+            base_HTML += "<div class=\"message right\">" + m[i][0] + "</div>"
+        }
+    }
+
+    //message_details.forEach(m => {
+    //    if (m[1] === 'flex-start') {
+    //        base_HTML += "<div class=\"message left\">" + m[0] + "</div>"
+    //    }
+    //    else if (m[1] === 'flex-end') {
+    //        base_HTML += "<div class=\"message right\">" + m[0] + "</div>"
+    //    }
+    //});
+
+    base_HTML += "</div>\
+            </div>\
+        </div>\"
+
+    console.log(base_HTML)
+
+    sendToAnki(message_details, base_HTML)
+}
+
+
+function sendToAnki(messages, chat_message_HTML) {
+
+    console.log("Sending to Anki...")
+
+    chrome.storage.local.get(['ankiDeckNameSelected', 'ankiNoteNameSelected', 'ankiFieldChatImage', 'ankiSelectedMessage', 'ankiConnectUrl'],
+        ({ ankiDeckNameSelected, ankiNoteNameSelected, ankiFieldChatImage, ankiSelectedMessage, ankiConnectUrl }) => {
+            url = ankiConnectUrl || 'http://localhost:8765';
+            model = ankiNoteNameSelected || 'Basic';
+            deck = ankiDeckNameSelected || 'Default';
+
+            console.log({
+                ankiDeckNameSelected,
+                ankiNoteNameSelected,
+                ankiFieldChatImage,
+                ankiSelectedMessage,
+                ankiConnectUrl
+            })
+
+            var fields = {
+                [ankiFieldChatImage]: chat_message_HTML,
+                [ankiSelectedMessage]: messages[0][0]
+            };
+
+            console.log(fields)
+
+            var body = {
+                "action": "addNote",
+                "version": 5,
+                "params": {
+                    "note": {
+                        "fields": fields,
+                        "modelName": model,
+                        "deckName": deck,
+                        "tags": ["Translator2Anki"]
+                    }
+                }
+            };
+            var permission_data = {
+                "action": "requestPermission",
+                "version": 6,
+            };
+
+            console.log("Fetching...")
+
+            fetch(url, {
+                method: "POST",
+                body: JSON.stringify(permission_data),
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log(data);
+                    fetch(url, {
+                        method: "POST",
+                        body: JSON.stringify(body),
+                    })
+                        .then((res) => res.json())
+                        .then((data) => {
+                            console.log("Fetch Return:")
+                            if (data.result === null) {
+                                alert("Error!\n" + data.error)
+                            }
+                            console.log("Sucess")
+                        }).catch((error) => console.log("EEROR! ", error));
+                })
+                .catch((error) => console.log("EEROR! ", error));
+            console.log("Sent to ANKI complete!\n");
+        });
+}
