a28ddd374fa943565a07be236a41efef319c9379
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2022-05-17 22:53:12 +0100

    Updated to use Manifest V3 function calls

--- background.js
+++ background.js
@@ -1,53 +1,56 @@
 /* backgound.js */
 console.log("----- [backgound.js] LOADED");
 
-const showOnPages = [
-	"*://*.instagram.com/direct/*",
-	"*://*.whatsapp.com/*",
-	"*://*.hellotalk.com/*"
-];
-
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse)
 {
 	console.log(request.message)
+	return true; // Inform Chrome that we will make a delayed sendResponse
 });
 
 function AddRightClickOption()
 {
 	console.log("[backgound.js] Adding Right Click Option");
 
-	chrome.contextMenus.create({
-		title: 'ANKI',
-		onclick: function (e)
+	// https://stackoverflow.com/questions/32718645/google-chrome-extension-add-the-tab-to-context-menu/32719354#32719354
+
+	chrome.contextMenus.removeAll(function ()
+	{
+		chrome.contextMenus.create({
+			id: "ANKI-MESSAGE-CONTEXT-MENU",
+			title: "ANKI",
+			contexts: ["selection"],
+			documentUrlPatterns: [
+				"*://*.instagram.com/direct/*",
+				"*://*.whatsapp.com/*",
+				"*://*.hellotalk.com/*"
+			]
+		});
+	});
+
+	chrome.contextMenus.onClicked.addListener(function (info, tab)
+	{
+		if (info.menuItemId == "ANKI-MESSAGE-CONTEXT-MENU")
 		{
-			chrome.tabs.query({
-				"active": true,
-				"currentWindow": true
-			}, function (tabs)
+			console.log(tab.url)
+			const url = tab.url
+			if (url.includes("instagram.com/direct"))
 			{
-				//console.log(tabs[0].url)
-				const url = tabs[0].url
-				if (url.includes("instagram.com/direct"))
-				{
-					message = "InstagramMessages"
-				}
-				else if (url.includes("web.hellotalk.com"))
-				{
-					message = "HelloTalkMessages"
-				}
-				else if (url.includes("web.whatsapp.com"))
-				{
-					message = "WhatsAppMessages"
-				}
-				console.log("Sending message: " + message)
-				chrome.tabs.sendMessage(tabs[0].id, {
-					"functiontoInvoke": message
-				});
+				message = "InstagramMessages"
+			}
+			else if (url.includes("web.hellotalk.com"))
+			{
+				message = "HelloTalkMessages"
+			}
+			else if (url.includes("web.whatsapp.com"))
+			{
+				message = "WhatsAppMessages"
+			}
+			console.log("Sending message: " + message)
+			chrome.tabs.sendMessage(tab.id, {
+				"functiontoInvoke": message
 			});
-		},
-		contexts: ["selection"],
-		documentUrlPatterns: showOnPages
-	}, () => { });
+		}
+	});
 }
 
 AddRightClickOption()
